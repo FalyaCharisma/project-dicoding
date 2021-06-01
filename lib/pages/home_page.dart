85df3f95eeab25a0b7b1915bf7fb1f9e8f7b73a4
= <=>
2021-06-01 18:39:57 +0700

    Membuat halaman home

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+ 
+  @override 
+  Widget build(BuildContext context){
+    
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData( 
+      primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Halaman Utama"), 
+        ),
+    
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(8), 
+            child: Column(
+              children: [
+                Column(children:[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Column(children: [
+                     Image.network("https://image.flaticon.com/icons/png/512/4746/4746422.png",  height: 150, width: 150,),
+                      Container(
+                        width: 170,
+                        child: RaisedButton(onPressed: (){
+                        },
+                          child: Text("Asia"),
+                        ),
+                      )
+                    ],
+                    ),
+                    Column(children: [
+                     Image.network("https://image.flaticon.com/icons/png/512/520/520031.png", height: 150, width: 150,),
+                      Container(
+                        width: 170, 
+                        child: RaisedButton(onPressed: (){
+                        },
+                          child: Text("Africa"),
+                        ),
+                      )
+                    ],
+                    ),
+                  ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Column(children: [
+                     Image.network("https://image.flaticon.com/icons/png/512/4746/4746422.png",  height: 150, width: 150,),
+                      Container(
+                        width: 170,
+                        child: RaisedButton(onPressed: (){
+                        },
+                          child: Text("Asia"),
+                        ),
+                      )
+                    ],
+                    ),
+                    Column(children: [
+                     Image.network("https://image.flaticon.com/icons/png/512/520/520031.png", height: 150, width: 150,),
+                      Container(
+                        width: 170, 
+                        child: RaisedButton(onPressed: (){
+                        },
+                          child: Text("Africa"),
+                        ),
+                      )
+                    ],
+                    ),
+                  ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Column(children: [
+                     Image.network("https://image.flaticon.com/icons/png/512/4746/4746422.png",  height: 150, width: 150,),
+                      Container(
+                        width: 170,
+                        child: RaisedButton(onPressed: (){
+                        },
+                          child: Text("Asia"),
+                        ),
+                      )
+                    ],
+                    ),
+                    Column(children: [
+                     Image.network("https://image.flaticon.com/icons/png/512/520/520031.png", height: 150, width: 150,),
+                      Container(
+                        width: 170, 
+                        child: RaisedButton(onPressed: (){
+                        },
+                          child: Text("Africa"),
+                        ),
+                      )
+                    ],
+                    ),
+                  ],
+                  ),
+                ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),     
+    );
+  }
+}
