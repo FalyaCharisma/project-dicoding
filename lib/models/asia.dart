610c92b9e04bab70b4239173fe21291d9cc20e23
= <=>
2021-06-03 19:31:06 +0700

    Membuat models untuk benua asia

---
+++ lib/models/asia.dart
@@ -0,0 +1,8 @@
+class Asia{
+  String nama;
+  String bendera;
+  String ibuKota;
+  String mataUang;
+
+  Asia({required this.nama, required this.bendera, required this.ibuKota, required this.mataUang});
+}
